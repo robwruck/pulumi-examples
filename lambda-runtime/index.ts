a039a3265510d09feb12b6c7fdc24f0b7658f7ae
Robert Wruck <r.wruck-extern@fielmann.com>
2023-08-21 11:57:15 +0200

    Set runtime to nodejs18.x

--- lambda-runtime/index.ts
+++ lambda-runtime/index.ts
@@ -6,12 +6,14 @@ const setupProject = async (): Promise<any> => {
     const awsConfig = new pulumi.Config('aws')
 
     const x86 = new ClientLambda("example_x86", {
+        runtime: 'nodejs18.x',
         architecture: "x86_64",
         memorySize: 128,
         timeout: 300
     })
 
     const arm = new ClientLambda("example_arm", {
+        runtime: 'nodejs18.x',
         architecture: "arm64",
         memorySize: 128,
         timeout: 300
