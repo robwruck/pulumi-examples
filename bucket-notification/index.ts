46f1c81c9d19f36c9fea66ccebcf03c710ff6b13
Robert Wruck <r.wruck-extern@fielmann.com>
2024-09-10 13:44:49 +0200

    Use pulumi project name as prefix

--- bucket-notification/index.ts
+++ bucket-notification/index.ts
@@ -1,3 +1,4 @@
+import * as pulumi from "@pulumi/pulumi"
 import * as aws from "@pulumi/aws"
 import { S3Bucket } from "./modules/S3Bucket";
 import { S3NotificationTopic } from "./modules/S3NotificationTopic";
@@ -7,28 +8,29 @@ import { DuplicateObjectLambda } from "./modules/DuplicateObjectLambda";
 
 const setupProject = async (): Promise<any> => {
 
+    const name = pulumi.getProject()
     const region = await aws.getRegion()
     const currentIdentity = await aws.getCallerIdentity()
 
-    const bucket = new S3Bucket('notification-bucket');
+    const bucket = new S3Bucket(`${name}-bucket`);
     
-    const topic = new S3NotificationTopic('notification-topic', {
+    const topic = new S3NotificationTopic(`${name}-topic`, {
         bucketName: bucket.bucket,
         bucketArn: bucket.arn,
         regionName: region.name,
         ownerAccountId: currentIdentity.accountId,
     })
     
-    const queue = new NotificationQueue('notification-queue', {
+    const queue = new NotificationQueue(`${name}-queue`, {
         topicArn: topic.arn
     })
 
-    const consumer = new NotificationConsumer('notification-consumer', {
+    const consumer = new NotificationConsumer(`${name}-consumer`, {
         queueArn: queue.arn,
         bucketArn: bucket.arn
     })
 
-    const duplicateObject = new DuplicateObjectLambda('duplicateObject')
+    const duplicateObject = new DuplicateObjectLambda(`${name}-duplicateObject`)
 
     return {
         bucketName: bucket.bucket,
