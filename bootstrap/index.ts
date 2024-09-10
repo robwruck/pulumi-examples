280a5e15f29910b4adc48afff2e3216cb9845239
Robert Wruck <r.wruck-extern@fielmann.com>
2024-09-10 14:07:01 +0200

    Configure common API gateway logging in bootstrap

--- bootstrap/index.ts
+++ bootstrap/index.ts
@@ -1,9 +1,13 @@
+import { ApiGatewayLogging } from "./modules/ApiGatewayLogging"
 import { PulumiS3Bucket } from "./modules/PulumiS3Bucket"
 
 const setupProject = async (): Promise<any> => {
 
     // Create an S3 bucket for storing the Pulumi stacks
     const bucket = new PulumiS3Bucket("pulumi")
+
+    // Set up account-wide logging role for API gateway
+    new ApiGatewayLogging("apigateway")
 
     return {
         bucketName: bucket.bucket,
