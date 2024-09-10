cc1465f7b3346a7d64c64c365b5dd97f81791228
Robert Wruck <r.wruck-extern@fielmann.com>
2024-09-10 13:36:00 +0200

    Use pulumi project name as prefix

--- objectlambda/index.ts
+++ objectlambda/index.ts
@@ -1,3 +1,4 @@
+import * as pulumi from "@pulumi/pulumi"
 import * as aws from "@pulumi/aws"
 import * as fs from "fs"
 import { S3KMSKey } from "./modules/S3KMSKey"
@@ -12,41 +13,42 @@ import { S3ReaderLambda } from "./modules/S3ReaderLambda"
 
 const setupProject = async (): Promise<any> => {
 
+    const name = pulumi.getProject()
     const currentIdentity = await aws.getCallerIdentity()
 
     // Create a custom KMS key
-    const key = new S3KMSKey('objectlambda-kms-key', {
+    const key = new S3KMSKey(`${name}-kms-key`, {
         ownerAccountId: currentIdentity.accountId
     })
 
     // Create the S3 bucket populated with some test files,
     // a role for bucket access and example reader lambdas
-    const bucket = new EncryptedS3Bucket('objectlambda-bucket', {
+    const bucket = new EncryptedS3Bucket(`${name}-bucket`, {
         ownerAccountId: currentIdentity.accountId,
         kmsKeyId: key.keyId
     })
 
     for (const fileName of fs.readdirSync('files')) {
-        new aws.s3.BucketObject(fileName, {
+        new aws.s3.BucketObject(`${name}-${fileName}`, {
             bucket: bucket.bucket,
             key: fileName,
             content: fs.readFileSync(`files/${fileName}`).toString()
         }, { parent: bucket })
     }
 
-    const bucketRole = new S3AccessRole('objectlambda-bucket-role', {
+    const bucketRole = new S3AccessRole(`${name}-bucket-role`, {
         ownerAccountId: currentIdentity.accountId,
         bucketArn: bucket.arn,
         kmsKeyArn: key.arn
     })
 
-    const bucketReader = new S3ReaderLambda('objectlambda-bucket-reader', {
+    const bucketReader = new S3ReaderLambda(`${name}-bucket-reader`, {
         roleArn: bucketRole.arn,
         runtime: 'nodejs16.x',
         handlerFileName: 'lambdas/getobject.js',
         bucketName: bucket.bucket
     })
-    const bucketReaderV3 = new S3ReaderLambda('objectlambda-bucket-reader-v3', {
+    const bucketReaderV3 = new S3ReaderLambda(`${name}-bucket-reader-v3`, {
         roleArn: bucketRole.arn,
         runtime: 'nodejs18.x',
         handlerFileName: 'lambdas/getobjectV3.js',
@@ -56,7 +58,7 @@ const setupProject = async (): Promise<any> => {
     // Now create an S3 access point,
     // a role for access and example reader lambdas.
     // The role will *not* be able to access the bucket directly.
-    const accessPoint = new AccessPoint('objectlambda-ap', {
+    const accessPoint = new AccessPoint(`${name}-ap`, {
         bucket: bucket.bucket,
         name: 'objectlambda-ap',
         publicAccessBlockConfiguration: {
@@ -67,19 +69,19 @@ const setupProject = async (): Promise<any> => {
         }
     })
 
-    const accessPointRole = new AccessPointRole('objectlambda-ap-role', {
+    const accessPointRole = new AccessPointRole(`${name}-ap-role`, {
         ownerAccountId: currentIdentity.accountId,
         accessPointArn: accessPoint.arn,
         kmsKeyArn: key.arn
     })
 
-    const accessPointReader = new S3ReaderLambda('objectlambda-ap-reader', {
+    const accessPointReader = new S3ReaderLambda(`${name}-ap-reader`, {
         roleArn: accessPointRole.arn,
         runtime: 'nodejs16.x',
         handlerFileName: 'lambdas/getobject.js',
         bucketName: accessPoint.arn
     })
-    const accessPointReaderV3 = new S3ReaderLambda('objectlambda-ap-reader-v3', {
+    const accessPointReaderV3 = new S3ReaderLambda(`${name}-ap-reader-v3`, {
         roleArn: accessPointRole.arn,
         runtime: 'nodejs18.x',
         handlerFileName: 'lambdas/getobjectV3.js',
@@ -89,12 +91,12 @@ const setupProject = async (): Promise<any> => {
     // Now create an S3 object lambda access point,
     // a role for access and example reader lambdas.
     // The role will *not* be able to access the bucket directly.
-    const lambdaFunction = new ObjectLambda('objectlambda-xform', {
+    const lambdaFunction = new ObjectLambda(`${name}-xform`, {
         ownerAccountId: currentIdentity.accountId,
         handlerFileName: "lambdas/xform.js"
     })
 
-    const lambdaAccessPoint = new ObjectLambdaAccessPoint('objectlambda-olap', {
+    const lambdaAccessPoint = new ObjectLambdaAccessPoint(`${name}-olap`, {
         name: 'objectlambda-olap',
         configuration: {
             supportingAccessPoint: accessPoint.arn,
@@ -111,7 +113,7 @@ const setupProject = async (): Promise<any> => {
         }
     })
 
-    const objectLambdaRole = new ObjectLambdaRole('objectlambda-access-role', {
+    const objectLambdaRole = new ObjectLambdaRole(`${name}-access-role`, {
         ownerAccountId: currentIdentity.accountId,
         accessPointArn: accessPoint.arn,
         objectLambdaAccessPointArn: lambdaAccessPoint.arn,
@@ -119,13 +121,13 @@ const setupProject = async (): Promise<any> => {
         kmsKeyArn: key.arn
     })
 
-    const objectLambdaReader = new S3ReaderLambda('objectlambda-reader', {
+    const objectLambdaReader = new S3ReaderLambda(`${name}-reader`, {
         roleArn: objectLambdaRole.arn,
         runtime: 'nodejs16.x',
         handlerFileName: 'lambdas/getobject.js',
         bucketName: lambdaAccessPoint.arn
     })
-    const objectLambdaReaderV3 = new S3ReaderLambda('objectlambda-reader-v3', {
+    const objectLambdaReaderV3 = new S3ReaderLambda(`${name}-reader-v3`, {
         roleArn: objectLambdaRole.arn,
         runtime: 'nodejs18.x',
         handlerFileName: 'lambdas/getobjectV3.js',
